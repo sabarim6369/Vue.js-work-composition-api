959b37413164fba69ce6876acd2b543249d58107
sabarim6369 <sabarim6369@gmail.com>
2025-07-02 00:30:26 +0530

    learn about getters in pinia

--- src/components/Store/index.js
+++ src/components/Store/index.js
@@ -1,16 +1,24 @@
 import { defineStore } from "pinia";
-const Mainstore=defineStore('main',{
-    state:()=>({
-        count:0
-    }),
-    actions:{
-        increasecount(){
-            this.count++;
-        },
-        decreasecount(){
-            if(this.count<1)return
-            this.count--;
-        }
-    }
-})
+const Mainstore = defineStore("main", {
+  state: () => ({
+    count: 0,
+    firstName: "Sabari",
+    lastName: "M",
+  }),
+  actions: {
+    increasecount() {
+      this.count++;
+    },
+    decreasecount() {
+      if (this.count < 1) return;
+      this.count--;
+    },
+  },
+  //getters are same like computed in vue.that is when its dependcy change it chang automatically accordingly
+  getters: {
+  fullName() {
+    return `${this.firstName} ${this.lastName}`
+  }
+}
+});
 export default Mainstore
